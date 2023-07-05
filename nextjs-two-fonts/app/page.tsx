8be4ceef82728e1b5cc62e6cd6141a46f9d848b5
AnastasiiaPirus <a.pirus2000@gmail.com>
2023-07-05 16:58:31 -0400

    Add code for "two fonts Nextjs" tutorial.

---
+++ nextjs-two-fonts/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <h1>Default</h1>
+      <h1 className="text1">Capriola</h1>
+      <h1 className="text2">Agdasima</h1>
+    </>
+  );
+}
