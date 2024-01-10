04c55fe097cf26976812ee05767366fde686240f
Anastasiia Pirus <86625391+AnastasiiaPirus@users.noreply.github.com>
2024-01-09 19:42:49 -0500

    Update Modal.tsx

--- modal-tutorial/app/components/Modal.tsx
+++ modal-tutorial/app/components/Modal.tsx
@@ -10,7 +10,7 @@ function Modal() {
     return (
         <>
             {modal &&
-                <div
+                <dialog
                     className="fixed left-0 top-0 w-full h-full bg-black bg-opacity-50 z-50 overflow-auto backdrop-blur flex justify-center items-center">
                     <div className="bg-white m-auto p-8">
                         <div className="flex flex-col items-center">
@@ -21,7 +21,7 @@ function Modal() {
                             </Link>
                         </div>
                     </div>
-                </div>
+                </dialog>
             }
         </>
     );
